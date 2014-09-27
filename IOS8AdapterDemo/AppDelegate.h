e8a553773f48e25c158d7c3b10904bccf1a5ee7c
skyming <tskyming@163.com>
2014-09-27 12:30:18 +0800

    Initial Commit

---
+++ IOS8AdapterDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOS8AdapterDemo
+//
+//  Created by skyming on 9/27/14.
+//  Copyright (c) 2014 Sensoro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOS8AdapterDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOS8AdapterDemo
+//
+//  Created by skyming on 9/27/14.
+//  Copyright (c) 2014 Sensoro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
